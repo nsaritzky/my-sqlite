f6d35589dd8a5374d3864260a9b4f4f44c693824
nsaritzky <nsaritzky@gmail.com>
2023-11-04 14:42:30 -0700

    Stage 3

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod parser;
 
-use anyhow::{bail, Result};
+use anyhow::{anyhow, bail, Result};
+use parser::parse_cell;
 use std::fs::File;
 use std::io::prelude::*;
+
+use crate::parser::parse_cell_pointers;
 
 fn main() -> Result<()> {
     // Parse arguments
@@ -21,18 +24,70 @@ fn main() -> Result<()> {
             let mut buf = Vec::new();
             file.read_to_end(&mut buf)?;
             let (rest, header) = parser::parse_header(&buf).map_err(|e| anyhow::anyhow!("{e}"))?;
-            let (_rest, page_header) =
+            let (rest, page_header) =
                 parser::parse_page_header(rest).map_err(|e| anyhow::anyhow!("{e}"))?;
-
-            // let mut header = [0; 100];
-            // file.read_exact(&mut header)?;
-
-            // // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
-            // #[allow(unused_variables)]
-            // let page_size = u16::from_be_bytes([header[16], header[17]]);
+            let (rest, cell_pointers) = parse_cell_pointers(rest, page_header.number_of_cells)
+                .map_err(|e| anyhow::anyhow!("{e}"))?;
 
             println!("database page size: {}", header.page_size);
             println!("number of tables: {}", page_header.number_of_cells);
+            println!("{:?}", cell_pointers);
+        }
+        ".tables" => {
+            let mut file = File::open(&args[1])?;
+            let mut buf = Vec::new();
+            file.read_to_end(&mut buf)?;
+            let header_slice = buf[0..100].to_vec();
+            let (rest, header) =
+                parser::parse_header(&header_slice).map_err(|e| anyhow::anyhow!("{e}"))?;
+            // for (i, page) in buf.chunks_mut(header.page_size as usize).enumerate() {
+            //     if i == 0 {
+            //         let page = &page[100..];
+            //         let (rest, page_header) =
+            //             parser::parse_page_header(page).map_err(|e| anyhow!("{e}"))?;
+            //         let (rest, cell_pointers) =
+            //             parse_cell_pointers(rest, page_header.number_of_cells)
+            //                 .map_err(|e| anyhow!("{e}"))?;
+            //         for p in cell_pointers {
+            //             let (_, cell) =
+            //                 parse_cell(&page[p as usize - 100..], page_header.page_type)
+            //                     .map_err(|e| anyhow!("{e}"))?;
+            //             println!("{:?}", cell);
+            //         }
+            //     } else {
+            //         let (rest, page_header) =
+            //             parser::parse_page_header(page).map_err(|e| anyhow!("{e}"))?;
+            //         let (rest, cell_pointers) =
+            //             parse_cell_pointers(rest, page_header.number_of_cells)
+            //                 .map_err(|e| anyhow!("{e}"))?;
+            //         for p in cell_pointers {
+            //             let (_rest, cell) = parse_cell(&page[p as usize..], page_header.page_type)
+            //                 .map_err(|e| anyhow!("{e}"))?;
+            //             println!("{:?}", cell);
+            //         }
+            //     }
+            // }
+            let db = &buf[100..];
+            let (rest, page_header) =
+                parser::parse_page_header(db).map_err(|e| anyhow::anyhow!("{e}"))?;
+            let (rest, cell_pointers) = parse_cell_pointers(rest, page_header.number_of_cells)
+                .map_err(|e| anyhow::anyhow!("{e}"))?;
+            let mut res = Vec::new();
+            for p in cell_pointers {
+                let (rest, cell) = parse_cell(&db[p as usize - 100..], page_header.page_type)
+                    .map_err(|e| anyhow!("{e}"))?;
+                match cell {
+                    parser::Cell::TableLeaf(content) => {
+                        if let parser::Data::Text(s) = content.payload[2].clone() {
+                            res.push(s);
+                        }
+                    }
+                    _ => bail!("Not a table leaf"),
+                }
+            }
+            for r in res {
+                print!("{r} ");
+            }
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
--- src/parser.rs
+++ src/parser.rs
@@ -1,12 +1,16 @@
-use anyhow::bail;
+use anyhow::{anyhow, bail};
 use nom::{
     bytes::complete::{tag, take},
-    number::complete::{be_u16, be_u32, be_u8},
+    combinator::{all_consuming, cond, consumed},
+    multi::{count, length_value},
+    number::complete::{
+        be_f64, be_i16, be_i24, be_i32, be_i64, be_i8, be_u16, be_u24, be_u32, be_u64, be_u8,
+    },
     sequence::{terminated, tuple},
     IResult,
 };
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Header {
     pub magic: String,
     pub page_size: u16,
@@ -95,26 +99,304 @@ pub fn parse_header(input: &[u8]) -> ParseResult<Header> {
 
 #[derive(Debug)]
 pub struct PageHeader {
-    pub page_type: u8,
+    pub page_type: PageType,
     pub first_freeblock_offset: u16,
     pub number_of_cells: u16,
     pub cell_content_area_offset: u16,
     pub fragmented_free_bytes: u8,
-    pub right_most_pointer: u32,
+    pub right_most_pointer: Option<u32>,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum PageType {
+    InteriorIndex,
+    InteriorTable,
+    LeafIndex,
+    LeafTable,
+}
+
+fn parse_page_type(input: &[u8]) -> ParseResult<PageType> {
+    let (rest, x) = be_u8(input)?;
+    match x {
+        2 => Ok((rest, PageType::InteriorIndex)),
+        5 => Ok((rest, PageType::InteriorTable)),
+        10 => Ok((rest, PageType::LeafIndex)),
+        13 => Ok((rest, PageType::LeafTable)),
+        _ => Err(nom::Err::Error(nom::error::Error::new(
+            input,
+            nom::error::ErrorKind::Fail,
+        ))),
+    }
 }
 
 pub fn parse_page_header(input: &[u8]) -> ParseResult<PageHeader> {
-    let mut parser = tuple((be_u8, be_u16, be_u16, be_u16, be_u8, be_u32));
-    let (rest, t) = parser(input)?;
-    Ok((
-        rest,
-        PageHeader {
-            page_type: t.0,
-            first_freeblock_offset: t.1,
-            number_of_cells: t.2,
-            cell_content_area_offset: t.3,
-            fragmented_free_bytes: t.4,
-            right_most_pointer: t.5,
-        },
-    ))
+    let (rest, page_type) = parse_page_type(input)?;
+    match page_type {
+        PageType::InteriorIndex | PageType::InteriorTable => {
+            let (rest, t) = tuple((parse_page_type, be_u16, be_u16, be_u16, be_u8, be_u32))(input)?;
+            Ok((
+                rest,
+                PageHeader {
+                    page_type: t.0,
+                    first_freeblock_offset: t.1,
+                    number_of_cells: t.2,
+                    cell_content_area_offset: t.3,
+                    fragmented_free_bytes: t.4,
+                    right_most_pointer: Some(t.5),
+                },
+            ))
+        }
+        _ => {
+            let (rest, t) = tuple((parse_page_type, be_u16, be_u16, be_u16, be_u8))(input)?;
+
+            Ok((
+                rest,
+                PageHeader {
+                    page_type: t.0,
+                    first_freeblock_offset: t.1,
+                    number_of_cells: t.2,
+                    cell_content_area_offset: t.3,
+                    fragmented_free_bytes: t.4,
+                    right_most_pointer: None,
+                },
+            ))
+        }
+    }
+}
+
+pub fn parse_cell_pointers(input: &[u8], number_of_cells: u16) -> ParseResult<Vec<u16>> {
+    let parser = take(number_of_cells as usize * 2);
+    let (rest, cells) = parser(input)?;
+    let mut res = Vec::<u16>::new();
+    for i in 0..number_of_cells {
+        res.push(u16::from_be_bytes([
+            cells[i as usize * 2],
+            cells[i as usize * 2 + 1],
+        ]));
+    }
+    Ok((rest, res))
+}
+
+fn parse_varint(input: &[u8]) -> ParseResult<i64> {
+    let mut res = 0;
+    let mut shift = 0;
+    let mut index = 0;
+    while index < 9 {
+        let (rest, byte) = be_u8(&input[index as usize..])?;
+        let byte = i64::from_be_bytes([0, 0, 0, 0, 0, 0, 0, byte]);
+        let add = byte & 0b0111_1111 as i64;
+        res = (res << shift) | add;
+        if byte & 0b1000_0000 == 0 {
+            return Ok((rest, res as i64));
+        }
+        shift += 7;
+        index += 1;
+    }
+    Err(nom::Err::Error(nom::error::Error::new(
+        input,
+        nom::error::ErrorKind::Fail,
+    )))
+}
+
+fn parse_positive_varint(input: &[u8]) -> ParseResult<i64> {
+    let mut res: i64 = 0;
+    let mut shift = 0;
+    loop {
+        let (rest, byte) = be_u8(input)?;
+        res |= (i64::from(byte) & 0b0111_1111) << shift;
+        if byte & 0b1000_0000 == 0 {
+            if res.is_positive() {
+                return Ok((rest, res as i64));
+            } else {
+                return Err(nom::Err::Error(nom::error::Error::new(
+                    input,
+                    nom::error::ErrorKind::Fail,
+                )));
+            }
+        }
+        shift += 7;
+    }
+}
+
+#[derive(Debug)]
+pub struct TableLeafCell {
+    pub row_id: i64,
+    pub payload: Vec<Data>,
+    pub overflow_page: Option<u32>,
+}
+
+#[derive(Debug)]
+pub struct TableInteriorCell {
+    left_child_page: u32,
+    row_id: u64,
+}
+
+#[derive(Debug)]
+pub struct IndexLeafCell {
+    payload: Vec<u8>,
+    overflow_page: Option<u32>,
+}
+
+#[derive(Debug)]
+pub struct IndexInteriorCell {
+    left_child_page: u32,
+    payload: Vec<u8>,
+    overflow_page: Option<u32>,
+}
+
+#[derive(Debug)]
+pub enum Cell {
+    TableLeaf(TableLeafCell),
+    TableInterior(TableInteriorCell),
+    IndexLeaf(IndexLeafCell),
+    IndexInterior(IndexInteriorCell),
+}
+
+fn does_overflow(u: usize, p: usize) -> bool {
+    false
+}
+
+pub fn parse_cell(input: &[u8], page_type: PageType) -> ParseResult<Cell> {
+    match page_type {
+        PageType::LeafTable => {
+            let (rest, payload_size) = parse_varint(input)?;
+            let (rest, row_id) = parse_varint(rest)?;
+            let (rest, payload) = take(payload_size as usize)(rest)?;
+            let (rest, overflow_page) = cond(does_overflow(0, 0), be_u32)(rest)?;
+            Ok((
+                rest,
+                Cell::TableLeaf(TableLeafCell {
+                    row_id,
+                    payload: parse_record(payload)?.1,
+                    // payload: payload.to_vec(),
+                    overflow_page,
+                }),
+            ))
+        }
+        PageType::InteriorTable => {
+            let (rest, left_child_page) = be_u32(input)?;
+            let (rest, row_id) = parse_varint(rest)?;
+            Ok((
+                rest,
+                Cell::TableInterior(TableInteriorCell {
+                    left_child_page,
+                    row_id: row_id as u64,
+                }),
+            ))
+        }
+        PageType::LeafIndex => {
+            let (rest, payload_size) = parse_varint(input)?;
+            let (rest, payload) = take(payload_size as usize)(rest)?;
+            let (rest, overflow_page) = cond(does_overflow(0, 0), be_u32)(rest)?;
+
+            Ok((
+                rest,
+                Cell::IndexLeaf(IndexLeafCell {
+                    //payload: all_consuming(parse_record)(payload)?.1,
+                    payload: payload.to_vec(),
+                    overflow_page,
+                }),
+            ))
+        }
+        PageType::InteriorIndex => {
+            let (rest, left_child_page) = be_u32(input)?;
+            let (rest, payload_size) = parse_varint(rest)?;
+            let (rest, payload) = take(payload_size as usize)(rest)?;
+            let (rest, overflow_page) = cond(does_overflow(0, 0), be_u32)(rest)?;
+
+            Ok((
+                rest,
+                Cell::IndexInterior(IndexInteriorCell {
+                    left_child_page,
+                    // payload: all_consuming(parse_record)(payload)?.1,
+                    payload: payload.to_vec(),
+                    overflow_page,
+                }),
+            ))
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Data {
+    Null,
+    Integer(i64),
+    Float(f64),
+    Text(String),
+    Blob(Vec<u8>),
+}
+
+pub fn parse_record(input: &[u8]) -> ParseResult<Vec<Data>> {
+    let (mut rest_outer, (bytes_consumed, header_size)) = consumed(parse_varint)(input)?;
+    let mut remaining_in_header = header_size - bytes_consumed.len() as i64;
+    let mut serial_types = Vec::new();
+    while remaining_in_header > 0 {
+        let (rest, (bytes_consumed, serial_type)) = consumed(parse_varint)(rest_outer)?;
+        rest_outer = rest;
+        serial_types.push(serial_type);
+        remaining_in_header -= bytes_consumed.len() as i64;
+    }
+    let mut res = Vec::new();
+    for s in serial_types {
+        match s {
+            0 => res.push(Data::Null),
+            1 => {
+                let (rest, x) = be_i8(rest_outer)?;
+                rest_outer = rest;
+                res.push(Data::Integer(x as i64));
+            }
+            2 => {
+                let (rest, x) = be_i16(rest_outer)?;
+                rest_outer = rest;
+                res.push(Data::Integer(x as i64));
+            }
+            3 => {
+                let (rest, x) = be_i24(rest_outer)?;
+                rest_outer = rest;
+                res.push(Data::Integer(x as i64));
+            }
+            4 => {
+                let (rest, x) = be_i32(rest_outer)?;
+                rest_outer = rest;
+                res.push(Data::Integer(x as i64));
+            }
+            5 => {
+                let (rest, xs) = count(be_u24, 2)(rest_outer)?;
+                let x = *xs.get(0).unwrap() as u64 + (*xs.get(1).unwrap() as u64) << 24;
+                rest_outer = rest;
+                res.push(Data::Integer(x as i64));
+            }
+            6 => {
+                let (rest, x) = be_i64(rest_outer)?;
+                rest_outer = rest;
+                res.push(Data::Integer(x as i64));
+            }
+            7 => {
+                let (rest, x) = be_f64(rest_outer)?;
+                rest_outer = rest;
+                res.push(Data::Float(x as f64));
+            }
+            8 => res.push(Data::Integer(0)),
+            9 => res.push(Data::Integer(1)),
+            s => {
+                if (s % 2 == 0) & (s >= 12) {
+                    let n = (s - 12) / 2;
+                    let (rest, x) = take(n as usize)(rest_outer)?;
+                    rest_outer = rest;
+                    res.push(Data::Blob(x.to_vec()));
+                } else if (s % 2 == 1) & (s >= 13) {
+                    let n = (s - 13) / 2;
+                    let (rest, x) = take(n as usize)(rest_outer)?;
+                    rest_outer = rest;
+                    res.push(Data::Text(String::from_utf8(x.to_vec()).unwrap()));
+                } else {
+                    Err(nom::Err::Error(nom::error::Error::new(
+                        input,
+                        nom::error::ErrorKind::Fail,
+                    )))?;
+                }
+            }
+        }
+    }
+    Ok((rest_outer, res))
 }
